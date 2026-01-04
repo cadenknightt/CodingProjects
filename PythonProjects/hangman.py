8bd1253fb78284322cf18999225386acb4b36b54
cadenknightt <99857412+cadenknightt@users.noreply.github.com>
2026-01-03 20:33:17 -0700

    Improve code framework
    Refactor hangman game for clarity and consistency.

--- PythonProjects/hangman.py
+++ PythonProjects/hangman.py
@@ -1,22 +1,19 @@
 # Imports random word generator
 from wonderwords import RandomWord
 
-# Main game function
+# Main hangman game function
 def hangman_game():
      print("\nWelcome to Hangman!")
 
-     # Computer chooses the word
+     # Computer chooses a word from the word generator
      d = RandomWord()
      word = d.word()
-     while word is None or not word.isalpha():
-           word = d.word()
 
-     # These attributes help the user know what words and letters have been guessed, and the starting number for lives left
-     word = word.upper()
+     # These variables set the game beginning and allow for updates throughout the game.
+     random_word = word.upper()
      guessed_letters = set()
      guessed_words = set()
      lives = 5
-
      hangman_stages = ["""
      -----------|
                 |
@@ -67,91 +64,88 @@ def hangman_game():
      """
      ]
 
-     # While lives are greater than 0 display the appropriate hangman visual and word display
+     # While lives > 0: display the hangman visual, display, and run game loop.
      while lives > 0:
           print(hangman_stages[5-lives])
-
-          display = ' '.join([char if char in guessed_letters else '_' for char in word])
+          display = ' '.join([char if char in guessed_letters else '_' for char in random_word])
           print(f"    {display}\n")
           print(f"LIVES REMAINING: {lives}")
           print(f"GUESSED LETTERS: {', '.join(sorted(guessed_letters)).upper() or 'NONE YET'}")
           print(f"GUESSED WORDS: {', '.join(sorted(guessed_words)).upper() or 'NONE YET'}\n")
 
-          if all(char in guessed_letters for char in word):
+          # If all the letters in guessed_letters are in random_word then break from loop.
+          if all(char in guessed_letters for char in random_word):
                break
 
           # Run the main loop to play the game
           while True:
                prompt = input("Are you guessing a letter or word (l/w): ")
                
-               # If user enters 'letter' or 'l', than provide the following prompts based on conditions
+               # If user enters 'letter' or 'l' in input
                if prompt in ["letter", "l"]:
                     while True:
-                         # Enter input for a single letter
-                         letter_selected = input("Enter a letter: ").strip().upper()
-                         # If input doesnt equal 1 number or is not a alphabetical
-                         if len(letter_selected) != 1 or not letter_selected.isalpha():
+                         # Enter input for a single valid letter.
+                         letter = input("Enter a letter: ").strip().upper()
+                         # If the letter doesnt have 1 value or is not a alphabetical value.
+                         if len(letter) != 1 or not letter.isalpha():
                               print("Please enter a single letter.\n")
                               continue
-                         # If input is already in guessed letters list, give an error
-                         if letter_selected in guessed_letters:
+                         # Give an error if the letter is already in guessed_letters list.
+                         if letter in guessed_letters:
                               print("You already guessed that letter.\n")
                               continue
-                         
-                         # Adds the input to a guessed letters list
-                         guessed_letters.add(letter_selected)
+                         # Adds the input to a guessed_letters list.
+                         guessed_letters.add(letter)
 
-                         # If input is the word
-                         if letter_selected in word:
-                              print(f"Good guess! '{letter_selected}' is in the word.\n")
+                         # If letter is in the random_word.
+                         if letter in random_word:
+                              print(f"Good guess! {letter} is in the word.\n")
                               break
-                         # If input is not the word
+                         # If letter is not in the random_word.
                          else:
-                              print(f"Good try! '{letter_selected}' is not in the word.\n")
+                              print(f"Good try! {letter} is not in the word.\n")
                               lives -= 1
                          break
                     break
 
-               # If user enters 'word' or 'w' in input, than provide the following prompts based on conditions
+               # If user enters 'word' or 'w' in input.
                elif prompt in ["word", "w"]:
                     while True:
                          # Enter input for word.
-                         word_selected = input("Enter a word: ").strip().upper()
+                         word = input("Enter a word: ").strip().upper()
                          # If input is less than two characters or is not alphabetical
-                         if len(word_selected) < 2 or not word_selected.isalpha():
+                         if len(word) < 2 or not word.isalpha():
                               print("Please enter a word with more than 2 letters.\n")
                               continue
                          # If input is in guessed words list, give an error
-                         if word_selected in guessed_words:
-                              print("You already guessed that word.\n")
+                         if word in guessed_words:
+                              print(f"You already guessed {word}.\n")
                               continue
+                         # Add input to guessed words list
+                         guessed_words.add(word)
 
-                         # Add input to guessed words list if not in there already
-                         guessed_words.add(word_selected)
-
-                         # If input = word, then congratulate and end game
-                         if word_selected == word:
-                              print("You guessed the word correctly!\n")
+                         # If input = word, then congratulate and end game.
+                         if word == random_word:
                               lives = 0
                               break
-                         # If the input does not match the correct word, give an error
+                         # If the input does not match the correct word, give an error.
                          else:
                               print("You guessed the word incorrectly.\n")
                               lives -= 2
                          break
                     break
 
-               # Give an error if neither options are satisfied
+               # If user doesn't enter 'letter'/'l' or 'word'/'w'.
                else:
                     print("Please enter 'letter'/'l', or 'word'/'w'.\n")
                     continue
      
-     # Options for when the word is correctly guessed or lost (0 lives)
-     if word in guessed_words or all(char in guessed_letters for char in word):
+     # Display message for when the user wins or loses either with words or letters.
+     if word in guessed_words or all(char in guessed_letters for char in random_word):
           print(f"""             
 |--------------------------------------------------------------
 |    Yay! You correctly guessed the word!
-|    The word was: {word}  
+|    The word was: {random_word}  
 |--------------------------------------------------------------
 """)
      else:
@@ -159,11 +153,11 @@ def hangman_game():
           print(f"""             
 |--------------------------------------------------------------
 |    You lost. Better luck next time.
-|    The word was: {word}      
+|    The word was: {random_word}      
 |--------------------------------------------------------------
 """)
 
-     # When the game is over, ask if the user wants to play again
+     # Ask if the user wants to play again when the game is over.
      while True:
           playagain = input("Would you like to play again? (y/n): ").strip().lower()
           if playagain in ["yes", "y"]:
